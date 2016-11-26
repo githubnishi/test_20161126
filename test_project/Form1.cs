a558223962cda772e49cbb26eefd648317f0fbc3
githubnishi <stl.akira.nishi@gmail.com>
2016-11-26 17:10:22 +0900

    保存と読み込み

--- test_project/Form1.cs
+++ test_project/Form1.cs
@@ -19,7 +19,39 @@ namespace test_project
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show(this, DateTime.Now.AddHours(10).ToString());
+            //MessageBox.Show(this, DateTime.Now.AddHours(10).ToString());
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Console.WriteLine(textBox1.text);
+
+
+            database.database.Instance.ExecuteSqlNonQuery("CREATE TABLE IF NOT EXISTS TEST(TITLE TEXT)");
+
+            var sql = new StringBuilder();
+            DataTable load = database.database.Instance.ExecuteSql("SELECT * FROM TEST");
+
+            if (load.Rows.Count != 0)
+            {
+                sql.AppendFormat("UPDATE TEST SET TITLE = '{0}'", textBox1.Text);
+            }else
+            {
+                sql.AppendFormat("INSERT INTO TEST VALUES('{0}')", textBox1.Text);
+            }
+            
+            database.database.Instance.ExecuteSqlNonQuery(sql.ToString());
+
+            //database.database.Instance.ExecuteSqlNonQuery("UPDATE TESTtbl SET TEST = " + textBox1.Text + "WHERE TEST = 1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable x = database.database.Instance.ExecuteSql("SELECT * FROM TEST");
+
+            textBox1.Text = x.Rows[0].ItemArray[0].ToString();
         }
     }
 } 
---
+++ test_project/database/database.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Windows.Forms;
+
+namespace test_project.database
+{
+    public class database
+    {
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        private static database instance = new database(Path.Combine(Application.StartupPath, "task.db"));
+
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        public static database Instance { get { return instance; } }
+
+        #region 
+
+        /// <summary>
+        /// コネクション
+        /// </summary>
+        private SQLiteConnection sqlConnection;
+
+        #endregion
+
+        /// <summary>
+        /// データベース
+        /// </summary>
+        public string DbFileName { get; set; }
+
+        /// <summary>
+        /// タイムアウト
+        /// </summary>
+        public int Timeout { get; set; }
+
+        #region コンストラクタ
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="dbFileName"></param>
+        private database(string dbFileName)
+        {
+            DbFileName = dbFileName;
+        }
+
+        #endregion
+
+        #region メソッド
+
+        /// <summary>
+        /// 接続
+        /// </summary>
+        /// <returns></returns>
+        public void Connect()
+        {
+            var scsb = new SQLiteConnectionStringBuilder();
+
+            scsb.DataSource = DbFileName;
+            scsb.DefaultTimeout = Timeout;
+
+            if (sqlConnection != null) Disconnect();
+            sqlConnection = new SQLiteConnection(scsb.ConnectionString);
+            sqlConnection.Open();
+        }
+
+        /// <summary>
+        /// 切断
+        /// </summary>
+        public void Disconnect()
+        {
+            if (sqlConnection != null && sqlConnection.State == ConnectionState.Open) sqlConnection.Close();
+        }
+
+        /// <summary>
+        /// SQL実行
+        /// </summary>
+        /// <param name="sql">SQL</param>
+        /// <returns></returns>
+        public DataTable ExecuteSql(string sql)
+        {
+            using (var sc = new SQLiteCommand(sql, sqlConnection))
+            {
+                using (SQLiteDataReader sdr = sc.ExecuteReader())
+                {
+                    var dt = new DataTable();
+                    dt.Load(sdr);
+                    return dt;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// SQL実行
+        /// </summary>
+        /// <param name="sql">SQL</param>
+        /// <returns></returns>
+        public int ExecuteSqlNonQuery(string sql)
+        {
+            using (var sc = new SQLiteCommand(sql, sqlConnection))
+            {
+                return sc.ExecuteNonQuery();
+            }
+        }
+
+        #endregion
+    }
+}
